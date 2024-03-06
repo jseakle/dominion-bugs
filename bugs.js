5557510819426f9b3b06a74ddc0c8d3316fbcf5a
jseakle <jseakle@gmail.com>
2024-03-06 01:17:29 -0500

    Update bugs.js

--- bugs.js
+++ bugs.js
@@ -1,7 +1,5 @@
-console.log('loaded')
 export function detect_bugs() {
     fetch("https://tipr.tk/static/dombugs.json").then((response) => response.json()).then((bugs) => {
-        console.log('foo')
         let nodes = document.getElementsByClassName('name-layer');
         let names = [];
         let alerts = [];
